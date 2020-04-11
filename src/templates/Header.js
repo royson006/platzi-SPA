a2d196009bad9488f60a6e868a91ede39a168825
Rodrigo <rlr1391@gmail.com>
2020-04-11 17:11:25 -0500

    Inicio SPA

---
+++ src/templates/Header.js
@@ -0,0 +1,17 @@
+const Header =()=>{
+    const view = `
+        <div class="Header-main">
+            <div class="Header-logo">
+                <h1>
+                    <a href="/">Prueba</a>
+                </h1>
+            </div>
+            <div class="Header-nav">
+                <a href="#/about/">about</a>
+            </div>
+        </div>
+    `;
+    return view;
+};
+
+export default Header;
